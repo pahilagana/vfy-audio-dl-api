62b8fd3dad6a4111ec6b8d24db46ebbff91cb93a
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-22 01:30:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const port = 3000;
 app.get('/download', async (req, res) => {
   try {
     const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
-    const title = req.query.title || 'audio'; // Get the title query parameter or use 'audio' as default
 
     if (!videoURL) {
       return res.status(400).send('Missing video URL');
     }
 
-    // Get information about the video (including file size)
+    // Get information about the video (including the title)
     const info = await ytdl.getInfo(videoURL);
-    const fileSize = info.formats[0].contentLength; // Get the file size in bytes
+    const videoTitle = info.videoDetails.title;
+    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
+    const sanitizedTitle = autoTitle || 'audio'; // Use the sanitized title or 'audio' as a default
 
-    // Set response headers to specify a downloadable file with the specified title
-    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    // Set response headers to specify a downloadable file with the auto-generated title
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp3"`);
     res.setHeader('Content-Type', 'audio/mpeg');
-   // res.setHeader('Content-Length', fileSize); // Set the Content-Length header
 
-    // Pipe the video stream into the response
+    // Pipe the audio stream into the response
     ytdl(videoURL, { filter: 'audioonly' }).pipe(res);
   } catch (error) {
     console.error('Error:', error);
@@ -33,3 +33,4 @@ app.get('/download', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+      
