ea4dc77f7176db29d1790e5f4027d56395f5030b
pahilagana <148686573+pahilagana@users.noreply.github.com>
2023-10-26 02:01:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const ytdl = require('ytdl-core');
 const app = express();
 const port = 3000;
 
-app.get('/download', async (req, res) => {
+// Route for downloading audio
+app.get('/download/audio', async (req, res) => {
   try {
     const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
 
@@ -11,21 +12,69 @@ app.get('/download', async (req, res) => {
       return res.status(400).send('Missing video URL');
     }
 
-    // Get information about the video (including the title and size)
+    // Get information about the video
     const info = await ytdl.getInfo(videoURL);
     const videoTitle = info.videoDetails.title;
     const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
     const sanitizedTitle = autoTitle || 'audio'; // Use the sanitized title or 'audio' as a default
     const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    const fileSize = audioFormats[0].contentLength || 'unknown'; // Get the audio size in bytes
 
-    // Set response headers to specify a downloadable audio file with the auto-generated title and size
+    // Select the best available audio format
+    const format = audioFormats[0];
+
+    if (!format) {
+      return res.status(404).send('No suitable audio format found');
+    }
+
+    // Get the content length (file size) of the audio
+    const contentLength = format.contentLength;
+
+    // Set response headers to specify a downloadable audio file with the auto-generated title
     res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}(vivek masona).mp3"`);
     res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('Content-Length', fileSize);
+    res.setHeader('Content-Length', contentLength); // Add content length to the headers
 
     // Pipe the audio stream into the response
-    ytdl(videoURL, { format: audioFormats[0] }).pipe(res);
+    ytdl(videoURL, { format }).pipe(res);
+
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Route for downloading video
+app.get('/download/video', async (req, res) => {
+  try {
+    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
+
+    if (!videoURL) {
+      return res.status(400).send('Missing video URL');
+    }
+
+    // Get information about the video
+    const info = await ytdl.getInfo(videoURL);
+    const videoTitle = info.videoDetails.title;
+    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
+    const sanitizedTitle = autoTitle || 'video'; // Use the sanitized title or 'video' as a default
+
+    // Select the best available video format
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+
+    if (!format) {
+      return res.status(404).send('No suitable video format found');
+    }
+
+    // Get the content length (file size) of the video
+    const contentLength = format.contentLength;
+
+    // Set response headers to specify a downloadable video file with the auto-generated title
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}(vivek masona).mp4"`);
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Content-Length', contentLength); // Add content length to the headers
+
+    // Pipe the video stream into the response
+    ytdl(videoURL, { format }).pipe(res);
 
   } catch (error) {
     console.error('Error:', error);
