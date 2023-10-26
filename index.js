b5f542e3f05419032c812f5e80a5402c736b7330
pahilagana <148686573+pahilagana@users.noreply.github.com>
2023-10-26 09:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.get('/download', async (req, res) => {
   }
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
 
