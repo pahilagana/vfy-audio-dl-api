27449556d7cb43406add907c70931345d01525b6
pahilagana <148686573+pahilagana@users.noreply.github.com>
2023-10-26 09:33:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,78 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 // Route for downloading audio
 app.get('/download/audio', async (req, res) => {
   try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
+    const videoURL = req.query.url;
 
     if (!videoURL) {
-      return res.status(400).send('Missing video URL');
+      return res.status(400).json({ error: 'Missing video URL' });
     }
 
-    // Get information about the video
     const info = await ytdl.getInfo(videoURL);
     const videoTitle = info.videoDetails.title;
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
-    const sanitizedTitle = autoTitle || 'audio'; // Use the sanitized title or 'audio' as a default
+    const sanitizedTitle = videoTitle.replace(/[^\w\s]/gi, '') || 'audio';
     const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
 
-    // Select the best available audio format
-    const format = audioFormats[0];
-
-    if (!format) {
-      return res.status(404).send('No suitable audio format found');
+    if (!audioFormats.length) {
+      return res.status(404).json({ error: 'No suitable audio format found' });
     }
 
-    // Get the content length (file size) of the audio
+    const format = audioFormats[0];
     const contentLength = format.contentLength;
 
-    // Set response headers to specify a downloadable audio file with the auto-generated title
-    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}(vivek masona).mp3"`);
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp3"`);
     res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('Content-Length', contentLength); // Add content length to the headers
+    if (contentLength) {
+      res.setHeader('Content-Length', contentLength);
+    }
 
-    // Pipe the audio stream into the response
     ytdl(videoURL, { format }).pipe(res);
-
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 // Route for downloading video
 app.get('/download/video', async (req, res) => {
   try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
+    const videoURL = req.query.url;
 
     if (!videoURL) {
-      return res.status(400).send('Missing video URL');
+      return res.status(400).json({ error: 'Missing video URL' });
     }
 
-    // Get information about the video
     const info = await ytdl.getInfo(videoURL);
     const videoTitle = info.videoDetails.title;
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
-    const sanitizedTitle = autoTitle || 'video'; // Use the sanitized title or 'video' as a default
-
-    // Select the best available video format
+    const sanitizedTitle = videoTitle.replace(/[^\w\s]/gi, '') || 'video';
     const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
 
     if (!format) {
-      return res.status(404).send('No suitable video format found');
+      return res.status(404).json({ error: 'No suitable video format found' });
     }
 
-    // Get the content length (file size) of the video
     const contentLength = format.contentLength;
 
-    // Set response headers to specify a downloadable video file with the auto-generated title
-    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}(vivek masona).mp4"`);
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp4"`);
     res.setHeader('Content-Type', 'video/mp4');
-    res.setHeader('Content-Length', contentLength); // Add content length to the headers
+    if (contentLength) {
+      res.setHeader('Content-Length', contentLength);
+    }
 
-    // Pipe the video stream into the response
     ytdl(videoURL, { format }).pipe(res);
-
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
